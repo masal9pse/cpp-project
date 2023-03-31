473d9b5dcf98722128c1060088b0682c8804d70d
山本大翔 <masato.yamamoto@sellca-sellcar.com>
2023-03-31 14:11:28 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    cout << "masatomsato" << endl;
+    // string data;
+    // getline(cin, data);
+    // cout << "Output: " << data << "\n\n";
+    return 0;
+}
